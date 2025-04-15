96ff1b5606c98926fdedd0e239f2c49f97541076
Mobilyst <2002derya2002@gmail.com>
2025-04-15 13:44:43 +0300

    explain json , crap , http method

--- FastApi/main.py
+++ FastApi/main.py
@@ -11,6 +11,11 @@ app=FastAPI()
 #farkli pathlari buradan tanimliyoruz /helloya gidince bu message degerimi gorecektir
 async def hello_world():
     return {"message": "hello world"}
+# return message dictinary dir json tarzi icin bu sekilde kullanilir
+# JSON (javascript object notation) nedir
+# veriyi ilettmek icin kullanilan gosterim yani dosya formati
+
+
 
 #terminalden su sekilde calistiriyoruz kodu
 # ilk olarak pip install uvicorn[standart] indiriyoruz
@@ -18,5 +23,12 @@ async def hello_world():
 # farkli calistirma yolu fastapi run main.py
 # calistirmayi kapatmak istersek CTRL+C tikliyoruz
 # pip freeze dedigimizde fast apinin kutuphaneyi kendi yukledigini gorceksiniz
+
+
+#crap nedir
+#crapin acilimi ve http methodlariyla eslesmesi  create(post) , read(get), update(put)  delete(delete)
+# http methodlari var client ve server tarafinda
+# http methodlari :get,post,put,head,delete,patch,options,connect,trace
+# sunucudan okuma islemi yapmak istiyorsak get kullanabilirz
 
 
