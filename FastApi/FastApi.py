fe0d25c3c14a716a1713570cddc15e046d25fe8d
Mobilyst <2002derya2002@gmail.com>
2025-04-15 12:39:50 +0300

    what is the fast api ?

---
+++ FastApi/FastApi.py
@@ -0,0 +1,19 @@
+# Fast API nedir
+# bir kutuphane web ugulamalar gelistirmemize yariyor
+# django python, flask python pythonla birlikte web uygulamalari gelistirilen
+# frameworklerdir backend kismini yazmamiz icin kullanilan kutuphanelerdir
+# fast api urun cikardigimizda hizli calisyor
+# hizli gelistirme yapabiliyorz bu framework ile
+#yapay zeka ile ilgili  urunler cikartirken fastapi kullaniliyor daha fazla
+
+
+# API nedir
+# uygulama programlama arabilirmidir
+# veritabaniniz var bazi bilgileriniz var bunun icinde
+# kullanicidda bu bilgilere mobilden webden ulasabilir
+# bir tane databaseden ulasiyoruz ama direkt o databaseden ulasmiyoruz
+# o arada yapilan is backend oluyor database ve uygulamalar arasinda bir baglanti kurmamiz gerekiyor
+# bu ilestirisimi API ile saglanir web urlsi gibidir backend icerisinde tanimlidir isteklere gore backende gider sonra database baglanir
+# rapidAPI sitesinde farkli apilar bulabilirsin
+
+
